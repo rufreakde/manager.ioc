e5f4d643f6bd124fde55416f427a15923c7486a0
rufreakde <rc@dev-six.com>
2020-02-18 17:54:20 +0100

    refactoring SetterUpdaterEditor

--- code/_propertyDrawer/Editor/SetterUpdaterEditor.cs
+++ code/_propertyDrawer/Editor/SetterUpdaterEditor.cs
@@ -6,12 +6,25 @@ using System.Reflection;
 [CustomEditor(typeof(MonoBehaviour), true)]
 public class SetterUpdaterEditor : Editor
 {
-    public override void OnInspectorGUI()
+    private string trimGeneratedSetterName(string _PropertyName)
     {
-        DrawDefaultInspector();
+        string prefix = _PropertyName.Substring(0, 4);
 
+        if (prefix != "set_")
+        {
+            _PropertyName = System.Char.ToLowerInvariant(_PropertyName[0]) + _PropertyName.Substring(1);
+        }
+        else
+        {
+            _PropertyName = System.Char.ToLowerInvariant(_PropertyName[4]) + _PropertyName.Substring(5);
+        }
+
+        return _PropertyName;
+    }
+
+    private void executeSetters()
+    {
         PropertyInfo[] Setters = serializedObject.targetObject.GetType().GetProperties();
-
         foreach (PropertyInfo setter in Setters)
         {
             try
@@ -19,24 +32,24 @@ public class SetterUpdaterEditor : Editor
                 MethodInfo setterMethodProperty = setter.GetSetMethod();
                 if (setterMethodProperty != null)
                 {
-                    string prefixCheck = setterMethodProperty.Name.Substring(0, 4);
-                    string propertyName = setterMethodProperty.Name;
-                    if (prefixCheck != "set_")
-                    {
-                        propertyName = System.Char.ToLowerInvariant(propertyName[0]) + propertyName.Substring(1);
-                    }
-                    else
-                    {
-                        propertyName = System.Char.ToLowerInvariant(setterMethodProperty.Name[4]) + setterMethodProperty.Name.Substring(5);
-                    }
-                    
-                    setterMethodProperty.Invoke(serializedObject.targetObject, new object[] { serializedObject.targetObject.GetType().GetField(propertyName, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(serializedObject.targetObject) });
+                    string propertyName = trimGeneratedSetterName(setterMethodProperty.Name);
+                    setterMethodProperty.Invoke(
+                        serializedObject.targetObject, new object[] {
+                            serializedObject
+                            .targetObject
+                            .GetType()
+                            .GetField(propertyName, BindingFlags.NonPublic | BindingFlags.Instance)
+                            .GetValue(serializedObject.targetObject)
+                        });
                 }
             }
-            catch(System.NullReferenceException nullError)
-            {
-                // do nothing the property is not defined...
-            }
+            catch{ } //ignore because property did not exist
         }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        executeSetters();
     }
 }
